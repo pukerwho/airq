386d29eb9d5cd35fe662369d4c0736bcd945e258
Puker Who <nawwesele@gmail.com>
2022-05-04 10:54:52 +0300

    edit

--- functions.php
+++ functions.php
@@ -133,6 +133,9 @@ add_action('do_feed_atom_comments', 'itsme_disable_feed', 1);
 remove_action( 'wp_head', 'feed_links_extra', 3 );
 remove_action( 'wp_head', 'feed_links', 2 );
 
+add_filter('use_block_editor_for_post', '__return_false', 10);
+
+
 /**
  * Register widget area.
  *
