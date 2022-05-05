934be0bdfd7f4574d1f819a44a25ded2f01aa288
Puker Who <nawwesele@gmail.com>
2022-05-05 14:58:49 +0300

    edit

--- template-parts/components/consultation.php
+++ template-parts/components/consultation.php
@@ -8,9 +8,9 @@
   <div>
     <?php get_template_part('template-parts/components/form-consultation'); ?>
   </div>
-  <div class="form_consultation_success"><?php _e("Дякую, ми отримали вашу заявку. Найближчим часом ми з вами зв'яжемося.", "airq"); ?></div>
 </div>
 <div class="w-full lg:w-8/12">
+  <div class="form_consultation_success hidden bg-orange-100 dark:bg-dark-xl rounded-lg p-4"><?php _e("Дякую, ми отримали вашу заявку. Найближчим часом ми з вами зв'яжемося.", "airq"); ?></div>
   <div>
     <?php _e("Запитання, пов'язані з обладнанням або продукцією, комерційні пропозиції та запити надсилайте на адресу", "airq"); ?> <span class="text-orange-400"><?php echo carbon_get_theme_option('crb_email'); ?>. </span><?php _e("Для вирішення термінових питань дзвоніть за телефоном", "airq"); ?> <?php echo carbon_get_theme_option('crb_phone'); ?>
   </div>
