c0bc0b7b6fa992dd83b88bd5257d6d21368e1fb8
Puker Who <nawwesele@gmail.com>
2022-05-09 11:15:30 +0300

    edit

--- template-parts/components/flavors/card.php
+++ template-parts/components/flavors/card.php
@@ -1,4 +1,4 @@
-<div class="relative mb-10 lg:mb-0">
+<div class="relative mb-10 lg:mb-6">
   <a href="<?php the_permalink(); ?>" class="absolute-link"></a>
   <div class="mb-4"><img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>" loading="lazy" class="w-full h-64 lg:h-96 object-cover"></div>
   <div class="text-lg text-center uppercase"><?php the_title(); ?></div>
