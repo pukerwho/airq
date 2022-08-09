148328e5220f6b897e9fd2d2a72d162825b8514a
Puker Who <nawwesele@gmail.com>
2022-08-09 14:54:05 +0300

    edit

--- functions.php
+++ functions.php
@@ -327,6 +327,35 @@ function create_taxonomy(){
   ) );
 }
 
+add_action('init', 'create_aromat_tags_taxonomy');
+function create_aromat_tags_taxonomy(){
+  register_taxonomy('aromat-tags', array('flavors'), array(
+    'label'                 => '', // определяется параметром $labels->name
+    'labels'                => array(
+      'name'              => 'Teги',
+      'singular_name'     => 'Teг',
+      'menu_name'         => 'Teги',
+    ),
+    'description'           => '', // описание таксономии
+    'public'                => true,
+    'publicly_queryable'    => null, // равен аргументу public
+    'show_in_nav_menus'     => true, // равен аргументу public
+    'show_ui'               => true, // равен аргументу public
+    'show_in_menu'          => true, // равен аргументу show_ui
+    'show_tagcloud'         => true, // равен аргументу show_ui
+    'show_in_rest'          => true, // добавить в REST API
+    'rest_base'             => null, // $taxonomy
+    'hierarchical'          => true,
+    'update_count_callback' => '',
+    //'query_var'             => $taxonomy, // название параметра запроса
+    'capabilities'          => array(),
+    'meta_box_cb'           => null, // callback функция. Отвечает за html код метабокса (с версии 3.8): post_categories_meta_box или post_tags_meta_box. Если указать false, то метабокс будет отключен вообще
+    'show_admin_column'     => false, // Позволить или нет авто-создание колонки таксономии в таблице ассоциированного типа записи. (с версии 3.5)
+    '_builtin'              => false,
+    'show_in_quick_edit'    => null, // по умолчанию значение show_ui
+  ) );
+}
+
 // Создаем счетчик для записей
 function tutCount($post_id) {
   
--- template-parts/components/flavors/card.php
+++ template-parts/components/flavors/card.php
@@ -1,5 +1,6 @@
-<div class="relative mb-10 lg:mb-6">
+<div class="relative mb-10">
   <a href="<?php the_permalink(); ?>" class="absolute-link"></a>
   <div class="mb-4"><img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="<?php the_title(); ?>" loading="lazy" class="w-full h-auto object-cover"></div>
-  <div class="text-lg text-center uppercase"><?php the_title(); ?></div>
+  <div class="text-lg text-center uppercase mb-2"><?php the_title(); ?></div>
+  <div class="text-center opacity-75"><?php echo carbon_get_the_post_meta('crb_flavors_subtitle'); ?></div>
 </div>
